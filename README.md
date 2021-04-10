8381276f00c8634dda7ccf8c9f907b65e3681f08
codingrev <emmowu10@gmail.com>
2021-04-10 02:47:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# manuel e-commerce
+ 
+
+HTML: HTML is used to create and save webdocument. E.g. Notepad/Notepad++
+CSS : (Cascading Style Sheets) Create attractiveLayout
+JavaScript: it is a programming language,commonly use with web browsers.
+Back end: PHP, MySQL
+
+PHP: Hypertext Preprocessor (PHP) is atechnology that allows software developers tocreate
+dynamically generated web pages, in HTML, XML,or other document types, as per client request.
+PHP is open source software.
+
+MySQL: MySql is a database, widely used foraccessing querying, updating, and managing datain databases.
+Software Requirement(any one)
+WAMP Server
+XAMPP Server
+MAMP Server
+LAMP Server
+Installation Steps
+Download zip file and Unzip file on your local server.
+Put this file inside “c:/wamp/www/” .
+Database Configuration:
+Open phpmyadmin
+Create Database named “store”.
+Import database store.sql.
+Open Your browser put inside URL:
+“http://localhost/store/”
+To Login as admin put inside URL:”http://localhost/store”
+
+Create and account and login
+
+
---
+++ settings.php
@@ -0,0 +1,62 @@
+<?php
+    session_start();
+    require 'connection.php';
+    if(!isset($_SESSION['email'])){
+        header('location:index.php');
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Manuel E-Commerce</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-xs-offset-0">
+                        <h1>Change Password</h1>
+                        <form method="post" action="setting_script.php">
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="oldPassword" placeholder="Old Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="newPassword" placeholder="New Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="retype" placeholder="Re-type new password">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Change">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+               <center>
+                   <p>Copyright &copy <script>document.write(new Date().getFullYear());</script> <a href="https://codingrev.netlify.com"> Manuel E-Commerce</a> Store. All Rights Reserved.</p>
+                   <p>This website is developed by Emmanuel Owusu (UEB1103619)</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
